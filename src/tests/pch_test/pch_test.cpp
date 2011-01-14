7bfe45673eef532de3f780c22a2952b03bdda94f
mat007 <mat007@860be788-9bd5-4423-9f1e-828f051e677b>
2011-01-14 17:04:43 +0000

    Added dummy application in order to bench the memory required for using the library in a precompiled header
    git-svn-id: https://svn.code.sf.net/p/turtle/code/trunk@186 860be788-9bd5-4423-9f1e-828f051e677b

---
+++ src/tests/pch_test/pch_test.cpp
@@ -0,0 +1,14 @@
+//
+//  Copyright Mathieu Champlon 2008
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include "pch_test.h"
+
+int main()
+{
+    return 0;
+}
