8b5623bc1d1279ee7c2cc63fdd5b33a30cebbb6b
mat007 <mat007@860be788-9bd5-4423-9f1e-828f051e677b>
2011-05-11 20:02:21 +0000

    Added automatic benchmark
    git-svn-id: https://svn.code.sf.net/p/turtle/code/trunk@324 860be788-9bd5-4423-9f1e-828f051e677b

---
+++ src/applications/turtle_bench/turtle_bench_0_class_10_max_args.cpp
@@ -0,0 +1,10 @@
+//
+//  Copyright Mathieu Champlon 2011
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#define MOCK_MAX_ARGS 10
+#include <turtle/mock.hpp>
