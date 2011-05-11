4f918457bcd8a051659e72a06d87c0ac26928d51
mat007 <mat007@860be788-9bd5-4423-9f1e-828f051e677b>
2011-05-11 16:09:59 +0000

    Cleanup
    git-svn-id: https://svn.code.sf.net/p/turtle/code/trunk@322 860be788-9bd5-4423-9f1e-828f051e677b

--- src/tests/turtle_test/max_args_test.cpp
+++ src/tests/turtle_test/max_args_test.cpp
@@ -17,27 +17,27 @@
 #define BOOST_LIB_NAME boost_unit_test_framework
 #include <boost/config/auto_link.hpp>
 
-#define TEXT(z, n, text) text
+#define IDENTITY(z, n, d) d
 
 namespace
 {
     struct my_custom_mock
     {
-        MOCK_METHOD_EXT( method, MOCK_MAX_ARGS, void( BOOST_PP_ENUM(MOCK_MAX_ARGS, TEXT, int) ), method )
-        MOCK_METHOD_EXT( method2, MOCK_MAX_ARGS, int( BOOST_PP_ENUM(MOCK_MAX_ARGS, TEXT, int) ), method2 )
+        MOCK_METHOD_EXT( method, MOCK_MAX_ARGS, void( BOOST_PP_ENUM(MOCK_MAX_ARGS, IDENTITY, int) ), method )
+        MOCK_METHOD_EXT( method2, MOCK_MAX_ARGS, int( BOOST_PP_ENUM(MOCK_MAX_ARGS, IDENTITY, int) ), method2 )
     };
 }
 
 BOOST_AUTO_TEST_CASE( call_mock_method_with_max_number_of_args )
 {
     my_custom_mock m;
-    MOCK_EXPECT( m, method ).once().with( BOOST_PP_ENUM(MOCK_MAX_ARGS, TEXT, 0) );
-    m.method( BOOST_PP_ENUM(MOCK_MAX_ARGS, TEXT, 0) );
+    MOCK_EXPECT( m, method ).once().with( BOOST_PP_ENUM(MOCK_MAX_ARGS, IDENTITY, 0) );
+    m.method( BOOST_PP_ENUM(MOCK_MAX_ARGS, IDENTITY, 0) );
 }
 
 BOOST_AUTO_TEST_CASE( call_mock_method_with_max_number_of_args_and_returning_something )
 {
     my_custom_mock m;
-    MOCK_EXPECT( m, method2 ).once().with( BOOST_PP_ENUM(MOCK_MAX_ARGS, TEXT, 0) ).returns( 42 );
-    BOOST_CHECK_EQUAL( 42, m.method2( BOOST_PP_ENUM(MOCK_MAX_ARGS, TEXT, 0) ) );
+    MOCK_EXPECT( m, method2 ).once().with( BOOST_PP_ENUM(MOCK_MAX_ARGS, IDENTITY, 0) ).returns( 42 );
+    BOOST_CHECK_EQUAL( 42, m.method2( BOOST_PP_ENUM(MOCK_MAX_ARGS, IDENTITY, 0) ) );
 }
