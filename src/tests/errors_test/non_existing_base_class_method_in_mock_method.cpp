7d28fc0d78a45af567348757cfe2d2abeef635b6
mat007 <mat007@860be788-9bd5-4423-9f1e-828f051e677b>
2011-05-18 21:15:18 +0000

    More test traces to analyse
    git-svn-id: https://svn.code.sf.net/p/turtle/code/trunk@335 860be788-9bd5-4423-9f1e-828f051e677b

---
+++ src/tests/errors_test/non_existing_base_class_method_in_mock_method.cpp
@@ -0,0 +1,22 @@
+//
+//  Copyright Mathieu Champlon 2011
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <turtle/mock.hpp>
+
+namespace
+{
+    struct my_base
+    {
+        virtual ~my_base() {}
+    };
+
+    MOCK_BASE_CLASS( my_class, my_base )
+    {
+        MOCK_METHOD( my_method, 0 )
+    };
+}
---
+++ src/tests/errors_test/number_of_arguments_greater_than_max_constant.cpp
@@ -0,0 +1,25 @@
+//
+//  Copyright Mathieu Champlon 2011
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <turtle/mock.hpp>
+
+BOOST_STATIC_ASSERT( MOCK_MAX_ARGS == 9 );
+
+namespace
+{
+    struct my_base
+    {
+        virtual ~my_base() {}
+        virtual void my_method( int, int, int, int, int, int, int, int, int, int ) = 0;
+    };
+
+    MOCK_BASE_CLASS( my_class, my_base )
+    {
+        MOCK_METHOD( my_method, 10 )
+    };
+}
