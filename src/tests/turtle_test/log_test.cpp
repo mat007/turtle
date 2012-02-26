6e1e22c55917bc86468fc20efe7e31f9b7722684
mat007 <mat007@860be788-9bd5-4423-9f1e-828f051e677b>
2012-02-26 12:21:39 +0000

    Added specialization to log unsigned chars as integers
    git-svn-id: https://svn.code.sf.net/p/turtle/code/trunk@413 860be788-9bd5-4423-9f1e-828f051e677b

--- src/tests/turtle_test/log_test.cpp
+++ src/tests/turtle_test/log_test.cpp
@@ -561,5 +561,5 @@ BOOST_AUTO_TEST_CASE( mock_detail_template_template_streamable_yields_its_value_
 
 BOOST_AUTO_TEST_CASE( unsigned_char_is_serialized_as_int )
 {
-    BOOST_CHECK_EQUAL( boost::lexical_cast< std::string >( int( 'a' ) ), to_string( unsigned char( 'a' ) ) );
+    BOOST_CHECK_EQUAL( boost::lexical_cast< std::string >( static_cast< int >( 'a' ) ), to_string< unsigned char >( 'a' ) );
 }
