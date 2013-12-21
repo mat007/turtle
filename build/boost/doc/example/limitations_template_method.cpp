e4e6d59a35ed44e281dff5b82dd9dfc1797eaf1e
mat007 <mat007@860be788-9bd5-4423-9f1e-828f051e677b>
2013-12-21 18:20:49 +0000

    Improved workaround for the template method limitation
    git-svn-id: https://svn.code.sf.net/p/turtle/code/trunk@702 860be788-9bd5-4423-9f1e-828f051e677b

--- build/boost/doc/example/limitations_template_method.cpp
+++ build/boost/doc/example/limitations_template_method.cpp
@@ -2,6 +2,8 @@
 #include <boost/test/auto_unit_test.hpp>
 #include <turtle/mock.hpp>
 
+namespace limitations_template_method_problem
+{
 //[ limitations_template_method_problem
 class concept
 {
@@ -12,15 +14,11 @@ public:
 };
 
 template< typename T >
-class client
+void function_under_test( T t ) // T is supposed to model the previous concept
 {
-public:
-    client( T t )             // T is supposed to model the previous concept
-    {
-        t.method( 42 );
-        t.method( "string" );
-    }
-};
+    t.method( 42 );
+    t.method( "string" );
+}
 //]
  
 //[ limitations_template_method_solution
@@ -30,4 +28,48 @@ MOCK_CLASS( mock_concept )
     MOCK_METHOD( method, 1, void( const char* ), method_string )
 };
 //]
+}
 
+namespace limitations_template_method_problem_2
+{
+//[ limitations_template_method_problem_2
+class concept
+{
+public:
+    template< typename T >
+    T create()
+    {
+        return T();
+    }
+};
+
+template< typename T >
+void function_under_test( T t ) // T is supposed to model the previous concept
+{
+    t.create< int >();
+    t.create< std::string >();
+}
+//]
+ 
+//[ limitations_template_method_solution_2
+MOCK_CLASS( mock_concept )
+{
+    template< typename T >
+    T create();
+
+    template<>
+    int create< int >()
+    {
+        return create_int();
+    }
+    template<>
+    std::string create< std::string >()
+    {
+        return create_string();
+    }
+
+    MOCK_METHOD( create_int, 0, int(), create_int )
+    MOCK_METHOD( create_string, 0, std::string(), create_string )
+};
+//]
+}
