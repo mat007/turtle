7cc5feeea0432f0bed4ab38aed31f71b4fc113b4
mat007 <mat007@860be788-9bd5-4423-9f1e-828f051e677b>
2014-05-20 05:56:37 +0000

    Updated documentation
    git-svn-id: https://svn.code.sf.net/p/turtle/code/trunk@731 860be788-9bd5-4423-9f1e-828f051e677b

---
+++ build/boost/doc/example/rationale.cpp
@@ -0,0 +1,20 @@
+// http://turtle.sourceforge.net
+//
+// Copyright Mathieu Champlon 2014
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include <turtle/mock.hpp>
+#include "calculator.hpp"
+#include "mock_view.hpp"
+
+//[ overflow_throws
+BOOST_AUTO_TEST_CASE( overflow_throws )
+{
+    mock_view v;
+    calculator c( v );
+    BOOST_CHECK_THROW( c.add( std::numeric_limits< int >::max(), 1 ), std::exception );
+}
+//]
