e41b75e525ff225c94071f3a502c8d9efdb6ef68
Mathieu Champlon <mathieu.champlon@masagroup.net>
2015-10-04 11:10:15 +0200

    Fixed error management

--- build/build.bat
+++ build/build.bat
@@ -6,13 +6,6 @@ rem Distributed under the Boost Software License, Version 1.0.
 rem (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
 
 setlocal
-goto Start
-
-:Error
-color 00
-goto :eof
-
-:Start
 
 rem error if BOOST_ROOT not set
 set BOOST=%BOOST_ROOT%
@@ -20,7 +13,7 @@ set BOOST=%BOOST_ROOT%
 pushd ..\test
 %BOOST%\b2 -q %*
 popd
-if errorlevel 1 goto Error
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 set BOOSTBOOK_DIR=..\bin\turtle\boostbook
 xcopy /Y /S /Q /I %BOOST%\tools\boostbook\xsl %BOOSTBOOK_DIR%\xsl
@@ -29,9 +22,8 @@ xcopy /Y /S /Q /I boostbook %BOOSTBOOK_DIR%
 xcopy /Y /S /Q /I %BOOST%\doc\src\boostbook.css ..\doc\html
 xcopy /Y /S /Q /I %BOOST%\doc\src\images\*.png ..\doc\html\images
 xcopy /Y /S /Q /I %BOOST%\doc\src\images\callouts\*.png ..\doc\html\images\callouts
-if errorlevel 1 goto Error
+if errorlevel 1 exit /b %ERRORLEVEL%
 pushd ..\doc
 %BOOST%\b2 -q %*
 popd
-
-:End
+if errorlevel 1 exit /b %ERRORLEVEL%
