9732acce78d025477b75e576bf29ec38a59c3a45
Mathieu Champlon <mathieu.champlon@masagroup.net>
2018-01-19 09:17:35 +0100

    Fixed travis cxxflags

--- build/build.sh
+++ build/build.sh
@@ -5,36 +5,28 @@
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
 
-run()
-{
-    echo $@
-    $@
-    r=$?
-    if test $r -ne 0 ; then
-        exit $r
-    fi
-}
-
 copy()
 {
     for dir; do true; done
-    run mkdir -p $dir
-    run cp $@
+    mkdir -p $dir
+    cp $@
 }
 
-run export BOOST=$BOOST_ROOT
+set -x
 
-run cd ../test
-run $BOOST/b2 -q "$@"
-run cd ../build
+export BOOST=$BOOST_ROOT
 
-run export BOOSTBOOK_DIR=../bin/turtle/boostbook
-run copy -r "$BOOST"/tools/boostbook/xsl $BOOSTBOOK_DIR
-run copy -r "$BOOST"/tools/boostbook/dtd $BOOSTBOOK_DIR
-run copy -r boostbook/* $BOOSTBOOK_DIR
-run copy "$BOOST"/doc/src/boostbook.css ../doc/html
-run copy "$BOOST"/doc/src/images/*.png ../doc/html/images
-run copy "$BOOST"/doc/src/images/callouts/*.png ../doc/html/images/callouts
-run cd ../doc
-run $BOOST/b2 -q "$@"
-run cd ../build
+cd ../test
+$BOOST/b2 -q "$@"
+cd ../build
+
+export BOOSTBOOK_DIR=../bin/turtle/boostbook
+copy -r "$BOOST"/tools/boostbook/xsl $BOOSTBOOK_DIR
+copy -r "$BOOST"/tools/boostbook/dtd $BOOSTBOOK_DIR
+copy -r boostbook/* $BOOSTBOOK_DIR
+copy "$BOOST"/doc/src/boostbook.css ../doc/html
+copy "$BOOST"/doc/src/images/*.png ../doc/html/images
+copy "$BOOST"/doc/src/images/callouts/*.png ../doc/html/images/callouts
+cd ../doc
+$BOOST/b2 -q "$C"
+cd ../build
