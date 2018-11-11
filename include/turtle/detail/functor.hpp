a3e954c0c9a5915933daa6b61d63b0a68efb5c89
David Vojtek <david.vojtek1@vsb.cz>
2018-11-11 18:29:55 +0100

    Resolve dependency on trivial_singleton.hpp from boost library

--- include/turtle/detail/functor.hpp
+++ include/turtle/detail/functor.hpp
@@ -15,38 +15,38 @@
 
 namespace mock
 {
-namespace detail
-{
-    class functor_mutex_t :
-        public boost::unit_test::singleton< functor_mutex_t >,
-        public mutex
-    {
-    private:
-        BOOST_TEST_SINGLETON_CONS( functor_mutex_t );
-    };
-    BOOST_TEST_SINGLETON_INST( functor_mutex )
+	namespace detail
+	{
+		class functor_mutex_t :
+			public boost::unit_test::singleton< functor_mutex_t >,
+			public mutex
+		{
+		private:
+			MOCK_SINGLETON_CONS(functor_mutex_t);
+		};
+		MOCK_SINGLETON_INST(functor_mutex)
 
-    template< typename Signature >
-    struct functor : function< Signature >
-    {
-        functor()
-        {
-            scoped_lock _( functor_mutex );
-            static functor* f = 0;
-            if( f )
-            {
-                *this = *f;
-                f = 0;
-                functor_mutex.unlock();
-            }
-            else
-            {
-                functor_mutex.lock();
-                f = this;
-            }
-        }
-    };
-}
+			template< typename Signature >
+		struct functor : function< Signature >
+		{
+			functor()
+			{
+				scoped_lock _(functor_mutex);
+				static functor* f = 0;
+				if (f)
+				{
+					*this = *f;
+					f = 0;
+					functor_mutex.unlock();
+				}
+				else
+				{
+					functor_mutex.lock();
+					f = this;
+				}
+			}
+		};
+	}
 } // mock
 
 #endif // MOCK_FUNCTOR_HPP_INCLUDED
--- include/turtle/detail/mutex.hpp
+++ include/turtle/detail/mutex.hpp
@@ -10,7 +10,8 @@
 #define MOCK_MUTEX_HPP_INCLUDED
 
 #include "../config.hpp"
-#include <boost/test/utils/trivial_singleton.hpp>
+#include <turtle/detail/singleton.hpp>
+
 #include <boost/shared_ptr.hpp>
 
 #ifdef MOCK_THREAD_SAFE
@@ -24,118 +25,118 @@
 
 namespace mock
 {
-namespace detail
-{
+	namespace detail
+	{
 #ifdef MOCK_HDR_MUTEX
-    typedef std::recursive_mutex mutex;
-    typedef std::lock_guard< mutex > scoped_lock;
+		typedef std::recursive_mutex mutex;
+		typedef std::lock_guard< mutex > scoped_lock;
 #else
-    typedef boost::recursive_mutex mutex;
-    typedef boost::lock_guard< mutex > scoped_lock;
+		typedef boost::recursive_mutex mutex;
+		typedef boost::lock_guard< mutex > scoped_lock;
 #endif
 
-    struct lock
-    {
-        lock( const boost::shared_ptr< mutex >& m )
-            : m_( m )
-        {
-            m_->lock();
-        }
-        lock( const lock& rhs )
-        {
-            m_.swap( rhs.m_ );
-        }
-        ~lock()
-        {
-            if( m_ )
-                m_->unlock();
-        }
+		struct lock
+		{
+			lock(const boost::shared_ptr< mutex >& m)
+				: m_(m)
+			{
+				m_->lock();
+			}
+			lock(const lock& rhs)
+			{
+				m_.swap(rhs.m_);
+			}
+			~lock()
+			{
+				if (m_)
+					m_->unlock();
+			}
 
-    private:
-        lock& operator=( const lock& rhs );
+		private:
+			lock& operator=(const lock& rhs);
 
-        mutable boost::shared_ptr< mutex > m_;
-    };
-}
+			mutable boost::shared_ptr< mutex > m_;
+		};
+	}
 } // mock
 
 #else // MOCK_THREAD_SAFE
 
 namespace mock
 {
-namespace detail
-{
-    struct mutex
-    {
-        mutex()
-        {}
-        void lock()
-        {}
-        void unlock()
-        {}
-    };
-    struct scoped_lock
-    {
-        scoped_lock( mutex& )
-        {}
-    };
-    struct lock
-    {
-        lock( const boost::shared_ptr< mutex >& )
-        {}
-    };
-}
+	namespace detail
+	{
+		struct mutex
+		{
+			mutex()
+			{}
+			void lock()
+			{}
+			void unlock()
+			{}
+		};
+		struct scoped_lock
+		{
+			scoped_lock(mutex&)
+			{}
+		};
+		struct lock
+		{
+			lock(const boost::shared_ptr< mutex >&)
+			{}
+		};
+	}
 } // mock
 
 #endif // MOCK_THREAD_SAFE
 
 namespace mock
 {
-namespace detail
-{
-    class error_mutex_t : public boost::unit_test::singleton< error_mutex_t >,
-        public mutex
-    {
-    private:
-        BOOST_TEST_SINGLETON_CONS( error_mutex_t );
-    };
-    BOOST_TEST_SINGLETON_INST( error_mutex )
+	namespace detail
+	{
+		class error_mutex_t : public boost::unit_test::singleton< error_mutex_t >,
+			public mutex
+		{
+		private:
+			MOCK_SINGLETON_CONS(error_mutex_t);
+		};
+		MOCK_SINGLETON_INST(error_mutex)
 
 #ifdef BOOST_MSVC
 #   pragma warning( push )
 #   pragma warning( disable: 4702 )
 #endif
-    template< typename Result, typename Error >
-    struct safe_error
-    {
-        static Result abort()
-        {
-            scoped_lock _( error_mutex );
-            return Error::abort();
-        }
-        template< typename Context >
-        static void fail( const char* message, const Context& context,
-            const char* file = "unknown location", int line = 0 )
-        {
-            scoped_lock _( error_mutex );
-            Error::fail( message, context, file, line );
-        }
-        template< typename Context >
-        static void call( const Context& context, const char* file, int line )
-        {
-            scoped_lock _( error_mutex );
-            Error::call( context, file, line );
-        }
-        static void pass( const char* file, int line )
-        {
-            scoped_lock _( error_mutex );
-            Error::pass( file, line );
-        }
-    };
+			template< typename Result, typename Error >
+		struct safe_error
+		{
+			static Result abort()
+			{
+				scoped_lock _(error_mutex);
+				return Error::abort();
+			}
+			template< typename Context >
+			static void fail(const char* message, const Context& context,
+				const char* file = "unknown location", int line = 0)
+			{
+				scoped_lock _(error_mutex);
+				Error::fail(message, context, file, line);
+			}
+			template< typename Context >
+			static void call(const Context& context, const char* file, int line)
+			{
+				scoped_lock _(error_mutex);
+				Error::call(context, file, line);
+			}
+			static void pass(const char* file, int line)
+			{
+				scoped_lock _(error_mutex);
+				Error::pass(file, line);
+			}
+		};
 #ifdef BOOST_MSVC
 #   pragma warning( pop )
 #endif
-}
+	}
 } // mock
 
 #endif // MOCK_MUTEX_HPP_INCLUDED
